017ae7b1f58c77ac5e8c4308ce9aa664ae9ef891
Ted Han <ted@knowtheory.net>
2017-05-14 15:11:56 -0700

    Maybe inside a sensible object.

--- app/bot/voxanon.rb
+++ app/bot/voxanon.rb
@@ -13,7 +13,7 @@ module Voxanon
             url = "https://process.text.audio/process"
             begin
               Rails.logger.info("Posting message (#{message.id}) to #{url}")
-              HTTParty.post(url, {fb_id: message.id, fb_audio_url: message.attachments.first["payload"]["url"]}.to_json)
+              HTTParty.post(url, body: {fb_id: message.id, fb_audio_url: message.attachments.first["payload"]["url"]}.to_json)
             end
             
             # tell the user that we'll send them a message after we've munged the audio
