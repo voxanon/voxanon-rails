9a87bfe1e0c4c4019c05c85e124e870ba0ec22b8
Ted Han <ted@knowtheory.net>
2017-05-14 15:14:07 -0700

    NOTIFY

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :messages do
-    post "/notify", to: "messages#notify"
-  end
+  post "/messages/notify", to: "messages#notify"
+  resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   mount Facebook::Messenger::Server, at: 'bot'
