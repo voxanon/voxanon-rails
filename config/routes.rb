62b4195e1b5619d801d8717b3f9954db935ee67f
Ted Han <ted@knowtheory.net>
2017-05-14 15:15:29 -0700

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/messages/notify", to: "messages#notify"
+  post "/messages_notify", to: "messages#notify"
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
