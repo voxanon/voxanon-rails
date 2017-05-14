35c6028cb2dce97f2b55fae9856a6561725d755c
Ted Han <ted@knowtheory.net>
2017-05-14 11:40:49 -0700

    Comment out failing thing.

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -5,9 +5,9 @@ unless Rails.env.production?
     bot_files.each{ |file| require_dependency file }
   end
 
-  ActionDispatch::Callbacks.to_prepare do
-    bot_reloader.execute_if_updated
-  end
+  #ActionDispatch::Callbacks.to_prepare do
+  #  bot_reloader.execute_if_updated
+  #end
 
   bot_files.each { |file| require_dependency file }
 end
