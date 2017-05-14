620fd54918b20561ce28b196048692e04bb6c9df
Ted Han <ted@knowtheory.net>
2017-05-14 14:57:03 -0700

    Message model instance from fb data.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,18 @@
 class Message < ApplicationRecord
+  
+  def self.new_from_fb(message)
+    attributes = {
+      fb_id:        message.id,
+      sender:       message.sender['id'],
+      seq:          message.seq,
+      sent_at:      message.sent_at,
+      text:         message.text,
+    }
+    unless message.attachments.nil? or message.attachments.empty?
+      attachment = attachments.first
+      attributes[:fb_audio_url] = attachment["url"] if attachment["type"] == "audio"
+    end
+
+    self.new(attributes)
+  end
 end
