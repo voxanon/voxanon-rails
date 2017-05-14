b920f936d8edcaa21848ff4883d92661566b5aaa
Ted Han <ted@knowtheory.net>
2017-05-14 14:57:41 -0700

    Listen for processed audio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,4 +7,10 @@ class MessagesController < ApplicationController
   def show
     render
   end
+  
+  def notify
+    Rails.logger.info(params.inspect)
+    message = Message.where(fb_id: params[:id]).first
+    # return 404 unless message
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages
+  resources :messages do
+    post "/notify", to: "messages#notify"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   mount Facebook::Messenger::Server, at: 'bot'
