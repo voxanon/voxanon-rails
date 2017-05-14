2cfe6006e31b0a873bc8a692269811bbb71c5a1c
Ted Han <ted@knowtheory.net>
2017-05-14 11:32:27 -0700

    bot code

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Voxanon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    # Auto-load the bot and its subdirectories
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
